a6f1e0f9c9b5bb7a6f907c88126edc6eec1fd8fb
Brandon Stewart <boveus@gmail.com>
2017-06-26 19:33:56 -0600

    working with minitests

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,9 +1,6 @@
 class Fizzbuzz
   def initialize(number)
   @number = number
-  def number
-    @number = number
-  end
 def fizzbuzzer()
   if @number % 105 == 0 then return "SuperFizzBuzz"
   elsif @number % 21 == 0 then return "SuperFizz"
@@ -12,7 +9,7 @@ def fizzbuzzer()
   elsif @number % 3 == 0 then return "Fizz"
   elsif @number % 5 == 0 then return "Buzz"
   elsif @number % 7 == 0 then return "Super"
-  else puts @number end
+  else return @number end
   end
 end
 end
--- super_fizz_test.rb
+++ super_fizz_test.rb
@@ -3,9 +3,32 @@ require 'minitest/autorun'
 require_relative 'super_fizz'
 
 class FizzTest < Minitest::Test
-  def supperfizzbuzztest
+  def test_superfizzbuzz
     fizzbuzz = Fizzbuzz.new(105)
-    result = fizzbuzz.fizzbuzzer
-    assert_equal 'SuperFizzBuzz', result
+    assert_equal "SuperFizzBuzz", fizzbuzz.fizzbuzzer
   end
+  def test_SuperFizz
+    fizzbuzz = Fizzbuzz.new(21)
+    assert_equal "SuperFizz", fizzbuzz.fizzbuzzer
+  end
+  def test_SuperBuzz
+    fizzbuzz = Fizzbuzz.new(35)
+    assert_equal "SuperBuzz", fizzbuzz.fizzbuzzer
+  end
+  def test_Fizz
+    fizzbuzz = Fizzbuzz.new(3)
+    assert_equal "Fizz", fizzbuzz.fizzbuzzer
+  end
+  def test_Buzz
+    fizzbuzz = Fizzbuzz.new(5)
+    assert_equal "Buzz", fizzbuzz.fizzbuzzer
+  end
+  def test_Super
+    fizzbuzz = Fizzbuzz.new(7)
+    assert_equal "Super", fizzbuzz.fizzbuzzer
+  end
+  def test_number
+    fizzbuzz = Fizzbuzz.new(61)
+    assert_equal 61, fizzbuzz.fizzbuzzer
+    end
 end
