cb776d085b77aeac709f3786c5fd19e5dd5129f8
Brandon Stewart <boveus@gmail.com>
2017-06-26 18:24:14 -0600

    fewer lines.

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,19 +1,12 @@
 
 (1..1000).each do |number|
-  if number % 3 == 0 && number % 5 == 0 && number % 7 == 0
-     puts "SuperFizzBuzz"
-  elsif number % 3 == 0 && number % 7 == 0
-     puts "SuperFizz"
-  elsif number % 5 == 0 && number % 7 == 0
-     puts "SuperBuzz"
-  elsif number % 3 == 0 && number % 5 == 0
-     puts "FizzBuzz"
-  elsif number % 3 == 0
-     puts "Fizz"
-  elsif number % 5 == 0
-     puts "Buzz"
-  elsif number % 7 == 0
-     puts "Super"
+  if number % 3 == 0 && number % 5 == 0 && number % 7 == 0 then puts "SuperFizzBuzz"
+  elsif number % 3 == 0 && number % 7 == 0 then puts "SuperFizz"
+  elsif number % 5 == 0 && number % 7 == 0 then puts "SuperBuzz"
+  elsif number % 3 == 0 && number % 5 == 0 then puts "FizzBuzz"
+  elsif number % 3 == 0 then puts "Fizz"
+  elsif number % 5 == 0 then puts "Buzz"
+  elsif number % 7 == 0 then puts "Super"
   else
      puts number
   end
