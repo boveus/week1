56fdbe661aea19644009f7bd8a8ef926390e551d
Brandon Stewart <boveus@gmail.com>
2017-06-26 18:42:39 -0600

    alternate solution

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,9 +1,9 @@
 
 (1..1000).each do |number|
-  if number % 3 == 0 && number % 5 == 0 && number % 7 == 0 then puts "SuperFizzBuzz"
-  elsif number % 3 == 0 && number % 7 == 0 then puts "SuperFizz"
-  elsif number % 5 == 0 && number % 7 == 0 then puts "SuperBuzz"
-  elsif number % 3 == 0 && number % 5 == 0 then puts "FizzBuzz"
+  if number % 105 == 0 then puts "SuperFizzBuzz"
+  elsif number % 21 == 0 then puts "SuperFizz"
+  elsif number % 35 == 0 then puts "SuperBuzz"
+  elsif number % 15 == 0 then puts "FizzBuzz"
   elsif number % 3 == 0 then puts "Fizz"
   elsif number % 5 == 0 then puts "Buzz"
   elsif number % 7 == 0 then puts "Super"
