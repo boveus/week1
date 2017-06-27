53698f409159f488473af858128a6380ebb08bfc
Brandon Stewart <boveus@gmail.com>
2017-06-26 18:24:53 -0600

    fewer still...

--- super_fizz.rb
+++ super_fizz.rb
@@ -7,7 +7,5 @@
   elsif number % 3 == 0 then puts "Fizz"
   elsif number % 5 == 0 then puts "Buzz"
   elsif number % 7 == 0 then puts "Super"
-  else
-     puts number
-  end
+  else puts number end
 end
