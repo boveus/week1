841f4351ee255dd4ee1e5f1e91cdfac8f6b8d63b
Brandon Stewart <boveus@gmail.com>
2017-06-26 18:50:45 -0600

    added a function

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,6 +1,6 @@
-
-(1..1000).each do |number|
-  if number % 105 == 0 then puts "SuperFizzBuzz"
+def fizzbuzzer(min,max)
+  (min..max).each do |number|
+  elsif number % 105 == 0 then puts "SuperFizzBuzz"
   elsif number % 21 == 0 then puts "SuperFizz"
   elsif number % 35 == 0 then puts "SuperBuzz"
   elsif number % 15 == 0 then puts "FizzBuzz"
@@ -8,4 +8,7 @@
   elsif number % 5 == 0 then puts "Buzz"
   elsif number % 7 == 0 then puts "Super"
   else puts number end
+  end
 end
+
+fizzbuzzer(0,1000)
